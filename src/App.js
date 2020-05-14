eaddf89cb1e62cb9c2678dfb79afe9718bf75bd3
joel5vega <joel5vega@gmail.com>
2020-05-14 16:20:02 -0400

    componente hijo

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Dropdown from "./components/Dropdown";
 //import Menu from "./components/menu";
 const items =[{id:1,value:"aula 1"},{id:2,value:"aula 2"}];
-let fuentes="http://127.0.0.1:8000/clases/show";
-const calendario=<Calendario fuente={fuentes} />
+// let fuentes="http://127.0.0.1:8000/clases/show";
+// const calendario=
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       loading: true,
       
-      fuente: fuentes,
+      fuente: "http://127.0.0.1:8000/clases/show"
       
     }
   }
   handleSelect=(evento)=>{
     console.log(evento)
+    var nuevaFuente = "http://127.0.0.1:8000/ambientes/"+evento;
+    this.setState({fuente:nuevaFuente})
   }
 
  
@@ -39,7 +41,7 @@ class App extends Component {
         </div>
         <div>
           <div className='calendario'>
-            {calendario}
+          <Calendario fuente={this.state.fuente} />
           </div>
 
         </div>
--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class NavItem extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {search: ''}
+    }
+
+    handleChange = (e) =>{
+        // console.log(e.target.value)
+        this.setState({search:e.target.value})
+    }
     render() {
         const { handleSelect } = this.props
 
         return (
             <div className="search-container">
-                <input className="search-input" type="text" />
-                <button className="search-btn" onClick={handleSelect}>ambiente</button>
+                <input 
+                value={this.state.search}
+                onChange={this.handleChange}
+                className="search-input" 
+                type="text" />
+                <button className="search-btn" onClick={()=>handleSelect(this.state.search)}>ambiente</button>
             </div>
         )
     }
