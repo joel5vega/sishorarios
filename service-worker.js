9b3cc40f39b9c68f81dedcd7648448681f771b13
joel5vega <joel5vega@gmail.com>
2022-03-29 09:57:25 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sishorarios/precache-manifest.1c33705fec769d516e86b2ff4ff662c2.js"
+  "/sishorarios/precache-manifest.40d9d4dee7ecb351a076726845896a36.js"
 );
 
 self.addEventListener('message', (event) => {
